6ad42b6295bf5aeff8255679c8b3bc26e389390a
YAMAZAKI2 <102411129+YAMAZAKI2@users.noreply.github.com>
2023-02-18 17:12:55 +0530

    first

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    localganache: {
-      url: process.env.PROVIDER_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY],
     }
   }, paths: {
     artifacts: "./client/src/artifacts"
